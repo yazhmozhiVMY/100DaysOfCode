e53beefc0ba5759897a25f04fa1bf0d39011bab7
Yazhmozhi VM <2473490@dundee.ac.uk>
2025-07-30 12:18:08 +0100

    Create Solution.java

---
+++ Day63_ZigZagConversion(LeetCode6)/Solution.java
@@ -0,0 +1,36 @@
+class Solution 
+{
+    public String convert(String s, int numRows) 
+    {
+        if (numRows == 1 || s.length() <= numRows) {
+            return s; // No zigzag needed
+        }
+
+        StringBuilder[] rows = new StringBuilder[numRows];
+        for (int i = 0; i < numRows; i++) {
+            rows[i] = new StringBuilder();
+        }
+
+        int currentRow = 0;
+        boolean goingDown = false;
+
+        for (char c : s.toCharArray()) {
+            rows[currentRow].append(c);
+
+            // Change direction if we're at the top or bottom row
+            if (currentRow == 0 || currentRow == numRows - 1) {
+                goingDown = !goingDown;
+            }
+
+            currentRow += goingDown ? 1 : -1;
+        }
+
+        // Combine all rows
+        StringBuilder result = new StringBuilder();
+        for (StringBuilder row : rows) {
+            result.append(row);
+        }
+
+        return result.toString();
+    }
+}
